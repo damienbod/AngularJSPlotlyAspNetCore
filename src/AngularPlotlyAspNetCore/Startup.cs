e1ae17d47bed68db4bb865c4c1d5d29a878bed89
damienbod <damien_bod@hotmail.com>
2016-05-20 18:47:52 +0200

    Updating the ASP.NET Core RC2 and ElasticsearchCrud RC2

--- src/AngularPlotlyAspNetCore/Startup.cs
+++ src/AngularPlotlyAspNetCore/Startup.cs
@@ -1,9 +1,15 @@
-﻿using Microsoft.AspNet.Builder;
-using Microsoft.AspNet.Hosting;
+﻿using AngularPlotlyAspNetCore.Providers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using AngularPlotlyAspNetCore.Providers;
+using Microsoft.AspNetCore.Http;
 
 namespace AngularPlotlyAspNetCore
 {
@@ -11,17 +17,32 @@ namespace AngularPlotlyAspNetCore
     {
         public Startup(IHostingEnvironment env)
         {
-            // Set up configuration sources.
-            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
-
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
             Configuration = builder.Build();
         }
 
+        
         public IConfigurationRoot Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add Cors support to the service
+            services.AddCors();
+
+            var policy = new Microsoft.AspNetCore.Cors.Infrastructure.CorsPolicy();
+
+            policy.Headers.Add("*");
+            policy.Methods.Add("*");
+            policy.Origins.Add("*");
+            policy.SupportsCredentials = true;
+
+            services.AddCors(x => x.AddPolicy("corsGlobalPolicy", policy));
+
             // Add framework services.
             services.AddMvc();
 
@@ -34,17 +55,50 @@ namespace AngularPlotlyAspNetCore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            app.UseCors("corsGlobalPolicy");
+
+            loggerFactory.AddConsole();
             loggerFactory.AddDebug();
 
-            app.UseIISPlatformHandler();
+            var angularRoutes = new[] {
+                "/overview",
+                "/region",
+                "/authorize",
+                "/unauthorized",
+                "/dataeventrecords",
+                "/dataeventrecords/create",
+                "/dataeventrecords/edit",
+                "/logoff",
+                "/securefile",
+                "/securefile/securefiles",
+            };
+
+            app.Use(async (context, next) =>
+            {
+                if (context.Request.Path.HasValue && null != angularRoutes.FirstOrDefault(
+                    (ar) => context.Request.Path.Value.StartsWith(ar, StringComparison.OrdinalIgnoreCase)))
+                {
+                    context.Request.Path = new PathString("/");
+                }
+
+                await next();
+            });
+            app.UseDefaultFiles();
 
             app.UseStaticFiles();
-
             app.UseMvc();
         }
 
-        // Entry point for the application.
-        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
     }
 }
